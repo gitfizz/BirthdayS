5424027f11cd1060c5e96c1dbc1bc5b326c41ab7
gitfizz <139196187+gitfizz@users.noreply.github.com>
2026-03-18 17:08:16 +0700

    Add files via upload

---
+++ Birthday.js
@@ -0,0 +1,123 @@
+let texts = [
+"Barakallah fii 'umrik Sarahh! 🎂🎈🎉🥳🥰",
+"Selamat hari raya juga 🌙🕋🕌",
+"Minal aidin wal faizin, Hafiz minta maaf yaa kalo ada salah 🙏🏻🙏🏻😭😭",
+"Proud Of You💖🥰"
+];
+
+let images = [
+"img1.jpg",
+"img2.jpg",
+"img3.jpg",
+"img4.jpg"
+];
+
+let index = 0;
+let autoslide;
+let typingInterval;
+let text = document.getElementById("text");
+let avatar = document.getElementById("avatar");
+let bar = document.getElementById("bar");
+
+let music1 = document.getElementById("music1");
+
+let popup1 = document.getElementById("popup1");
+let popup2 = document.getElementById("popup2");
+
+let open1 = document.getElementById("pesan1");
+let close1 = document.getElementById("close1");
+
+let open2 = document.getElementById("pesan2");
+let close2 = document.getElementById("close2");
+
+/* TYPING EFFECT */
+
+function typingEffect(sentence){
+
+let i = 0;
+text.innerHTML = "";
+
+typingInterval = setInterval(function(){
+text.innerHTML += sentence.charAt(i);
+i++;
+
+if(i >= sentence.length){
+clearInterval(typingInterval);
+}
+
+},50);
+
+}
+
+function slide(){
+
+bar.style.transition = "none";
+bar.style.width = "0%";
+
+setTimeout(()=>{
+bar.style.transition = "width 4.8s";
+bar.style.width = "100%";
+},50);
+
+typingEffect(texts[index]);
+avatar.src = images[index];
+
+if(index < texts.length - 1){
+    index++;
+}else{
+    clearInterval(autoslide);
+}
+
+}
+
+open1.onclick = function(){
+
+popup1.classList.add("active");
+music1.play();
+index = 0;
+slide();
+autoslide = setInterval(slide,5000);
+}
+
+close1.onclick = function(){
+popup1.classList.remove("active");
+clearInterval(autoslide);
+clearInterval(typingInterval);
+}
+
+open2.onclick = function(){
+
+popup2.classList.add("active");
+popup1.classList.remove("active");
+clearInterval(autoslide);
+clearInterval(typingInterval);
+
+}
+
+close2.onclick = function(){
+popup2.classList.remove("active");
+}
+
+
+/* LOVE ANIMATION */
+
+const hearts = document.querySelector(".hearts");
+
+function createHeart(){
+
+const heart = document.createElement("span");
+
+heart.innerHTML = "❤️";
+
+heart.style.left = Math.random()*100 + "%";
+heart.style.fontSize = Math.random()*20 + 10 + "px";
+
+hearts.appendChild(heart);
+
+setTimeout(()=>{
+heart.remove();
+},10000);
+
+}
+
+setInterval(createHeart,300);
